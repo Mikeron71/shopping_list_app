d1230dc55ee14e79f957937122a0e70e2c0763c1
Michaël Rondeau <rondeau_6@hotmail.com>
2023-01-22 18:46:51 -0500

    background , state

--- shopping_list_app/lib/Pages/shopping_list/shopping_list_page.dart
+++ shopping_list_app/lib/Pages/shopping_list/shopping_list_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/Pages/shopping_list/top_banner.dart';
 
@@ -10,9 +12,9 @@ class ShoppingListPage extends StatefulWidget {
 
 class _ShoppingListPageState extends State<ShoppingListPage> {
   List<ShoppingItem> foodItems = [
-    ShoppingItem("Patate"),
-    ShoppingItem("Pizza"),
-    ShoppingItem("Pain")
+    ShoppingItem("Patate", false),
+    ShoppingItem("Pizza", false),
+    ShoppingItem("Pain", false)
   ];
 
   final textc = TextEditingController();
@@ -20,7 +22,7 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
   addToList() {
     if (textc.text != "") {
       setState(() {
-        foodItems.add(ShoppingItem(textc.text));
+        foodItems.add(ShoppingItem(textc.text, false));
         textc.clear();
         FocusScope.of(context).unfocus();
       });
@@ -30,33 +32,56 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        TopBanner(),
-        Expanded(
-          child: ListView.builder(
-            itemCount: foodItems.length,
-            itemBuilder: (context, index) {
-              return Dismissible(
-                  key: UniqueKey(),
-                  child: foodItems[index],
-                  onDismissed: (direction) => setState(() {
+        body: Stack(
+      children: [
+        Image.asset("assets/images/food.png",
+            fit: BoxFit.fill, height: double.infinity, width: double.infinity),
+        Column(
+          children: <Widget>[
+            const TopBanner(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: foodItems.length,
+                itemBuilder: (context, index) {
+                  final item = foodItems[index];
+                  return Dismissible(
+                    // Each Dismissible must contain a Key. Keys allow Flutter to
+                    // uniquely identify widgets.
+                    key: Key(item.itemName),
+                    // Provide a function that tells the app
+                    // what to do after an item has been swiped away.
+                    onDismissed: (direction) {
+                      // Remove the item from the data source.
+                      setState(() {
                         foodItems.removeAt(index);
-                      }));
-            },
-          ),
-        ),
-        TextField(
-          controller: textc,
-          decoration: InputDecoration(
-              border: const OutlineInputBorder(),
-              hintText: 'Add an item',
-              suffixIcon: IconButton(
-                  onPressed: () {
-                    addToList();
-                  },
-                  icon: const Icon(Icons.add))),
-        ),
+                      });
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("${item.itemName} deleted")));
+                    },
+                    child: item,
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: TextField(
+                controller: textc,
+                decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Add an item',
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          addToList();
+                        },
+                        icon: const Icon(Icons.add))),
+              ),
+            ),
+          ],
+        )
       ],
     ));
   }
@@ -64,25 +89,25 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
 
 class ShoppingItem extends StatefulWidget {
   final String itemName;
+  bool? _coche;
 
-  const ShoppingItem(this.itemName, {super.key});
+  ShoppingItem(this.itemName, this._coche, {super.key});
 
   @override
   State<ShoppingItem> createState() => _ShoppingItemState();
 }
 
 class _ShoppingItemState extends State<ShoppingItem> {
-  bool _coche = false;
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: const Color.fromARGB(255, 187, 246, 189),
+      color: Colors.white,
       child: CheckboxListTile(
-          value: _coche,
+          value: widget._coche,
           title: Text(widget.itemName),
-          onChanged: (bool? value) {
+          onChanged: (bool? newValue) {
             setState(() {
-              _coche = !_coche;
+              widget._coche = newValue;
             });
           }),
     );
--- shopping_list_app/lib/main.dart
+++ shopping_list_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/Pages/shopping_list/shopping_list_page.dart';
 
+import 'navigation_widget.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,61 +17,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: _title,
       home: NavigationWidget(),
-    );
-  }
-}
-
-class NavigationWidget extends StatefulWidget {
-  const NavigationWidget({super.key});
-
-  @override
-  State<NavigationWidget> createState() => _NavigationWidgetState();
-}
-
-class _NavigationWidgetState extends State<NavigationWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static final List<Widget> _widgetOptions = <Widget>[
-    ShoppingListPage(),
-    const Text(
-      'Index 1: Business',
-      style: optionStyle,
-    )
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Shop-It')),
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag_outlined),
-            label: 'List',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: '???',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        backgroundColor: Colors.green,
-        selectedItemColor: Colors.white,
-        onTap: _onItemTapped,
-      ),
     );
   }
 }
